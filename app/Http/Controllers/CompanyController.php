fe5789ee1f414aa8bc34ef03e592e14fb707dbc2
Marcelo Carvalho <49615369+marcelocarvalhoqueiroz@users.noreply.github.com>
2026-07-15 16:34:45 -0300

    minor - Importação dos Models nos controllers.

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
--- app/Http/Controllers/TechController.php
+++ app/Http/Controllers/TechController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tech;
 use Illuminate\Http\Request;
 
 class TechController extends Controller
@@ -25,7 +26,7 @@ class TechController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Tech $tech)
     {
         //
     }
@@ -33,7 +34,7 @@ class TechController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Tech $tech)
     {
         //
     }
@@ -41,7 +42,7 @@ class TechController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Tech $tech)
     {
         //
     }
